004d6e90c6fdadb24676f0d1b4a8b091e6fc12ec
sebastian <rockmadero@gmail.com>
2025-04-09 14:07:25 -0500

    primera pagina y disponibilidad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      favicon: "favicon.png",
+      logo: {
+        src: "./src/assets/geh_large_logo.png",
+      },
+      title: "API docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/innovacion00/agencias-api-doc",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Disponibilidad",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Explicacion", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
